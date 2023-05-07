5e62651ee34fa921996b9db637f61deded1c11e9
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-05-07 16:23:34 -0700

    made my models

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    
+      username: {
+        type: String,
+        unique: true, 
+        required: true,
+        trim: true
+      },
+      email: {
+        type: String, 
+        required: true,
+        unique: true, 
+        validate: {
+            validator: () => Promise.resolve(false),
+            message: 'Email validation failed'
+          }
+      },
+      friends: [
+        {
+            type: Schema.Types.ObjectId
+        }
+      ],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `fullName` that gets and sets the user's full name
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  })
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,8 @@
+const { User } = require('./User');
+const { Thought, reactionSchema } = require('./Thought');
+
+// use User and Thought as needed
+
+
+
+module.exports = { User, Thought, reactionSchema };
